0d5283c1b812d6f41ab374b71b78dc28ed27f1c3
nayitovivero <nayitovivero@gmail.com>
2025-03-09 14:34:47 -0500

    correcion

---
+++ Lenguaje C/Nayeli_1946-1/selectiva/simplee2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int num,div,r;
+
+    cout<<"ingresa un numero:";
+    cin>>num;
+    cout <<"ingresa un numero divisor";
+    cin >> div;
+    r= num % div;
+    if(r==0){
+        cout <<"el numero"<< num << "es divible por" << div << endl;
+        
+    }
+    
+    {
+         cout <<"el numero"<< num << "es divible por" << div << endl;
+    }
+    num = 0;
+}
